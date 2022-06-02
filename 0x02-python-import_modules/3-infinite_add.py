01c3965adb22ce7ba5c0df16e2239270e5d9421b
unknown <aklileseyoum294@gmail.com>
2022-06-03 00:46:51 +0300

    commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-s =  0
-for i in range(1, len(argv)):
-    s = s + int(argv[i])
-print(s)
+if __name__ == "__main__":
+    from sys import argv
+    s =  0
+    for i in range(1, len(argv)):
+        s = s + int(argv[i])
+    print(s)
