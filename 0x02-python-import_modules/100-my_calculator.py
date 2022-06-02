3a6acca12ae58ee86223611afb98a68f1241a27c
unknown <aklileseyoum294@gmail.com>
2022-06-03 01:49:38 +0300

    commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,22 +4,28 @@ if __name__ == "__main__":
     from sys import argv
     if len(argv) == 1:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        print(exit(1))
+        print(1)
+        exit(1)
     a = int(argv[1])
     b = int(argv[3])
     n = argv[2]
     if n == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
+        print(0)
         exit(0)
     elif n == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
+        print(0)
         exit(0)
     elif n == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
+        print(0)
         exit(0)
     elif n == '/':
         print("{} / {} = {}".format(a, b, div(a, b)))
+        print(0)
         exit(0)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
+        print(1)
         exit(1)
