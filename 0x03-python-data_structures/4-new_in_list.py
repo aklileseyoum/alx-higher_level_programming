a52554c669430b601f9450f2267178126e641eb7
unknown <aklileseyoum294@gmail.com>
2022-06-06 21:33:47 +0300

    commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     new_list = []
     for i in range(len(my_list)):
         new_list.append(my_list[i])
-    if idx > 0 and idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
         new_list[idx] = element
     return new_list
