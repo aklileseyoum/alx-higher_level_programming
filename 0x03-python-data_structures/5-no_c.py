5fc46969bc6b4095dcf3d8dde17fe1a4958ffb13
unknown <aklileseyoum294@gmail.com>
2022-06-06 21:44:50 +0300

    commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for i in range(len(my_string)):
-        if my_string[i] == 'c':
+        if my_string[i] == 'c' or my_string[i] == 'C':
             for j in range(i, len(my_string)):
                 my_string[j] = my_string[j + 1]
     return my_string
