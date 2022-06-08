adf4c7ea78c5c190fbc53b536dbf4461432029ee
unknown <aklileseyoum294@gmail.com>
2022-06-08 18:00:07 +0300

    commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_directory:
-        return "None"
-    values = a_dictionary.values()
-    high = 0
-    for i in values:
-        if i > high:
-            high = i
-    for k, v in a_dictionary.items():
-        if high == v:
-            return k
+    if not a_dictionary:
+        return None
+    biggest = 0
+    for key, value in a_dictionary.items():
+        if value > biggest:
+            biggest = value
+    for key, value in a_dictionary.items():
+        if value == biggest:
+            return key
