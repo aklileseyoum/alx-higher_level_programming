5b6a16f33e81d1a4257c3bdd115c55dc92776697
unknown <aklileseyoum294@gmail.com>
2022-06-08 18:23:05 +0300

    12. Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    sum = 0
+    for i in range(len(roman_string)):
+        if roman_string[i] == 'I':
+            if i != len(roman_string) - 1:
+                if roman_string[i+1] == 'I':
+                    sum += 1
+                else:
+                    sum -= 1
+            else:
+                sum += 1
+        elif roman_string[i] == 'V':
+            sum += 5
+        elif roman_string[i] == 'X':
+            sum += 10
+        elif roman_string[i] == 'L':
+            sum += 50
+        elif roman_string[i] == 'C':
+            sum += 100
+        elif roman_string[i] == 'D':
+            sum += 500
+        elif roman_string[i] == 'M':
+            sum += 1000
+    return sum
