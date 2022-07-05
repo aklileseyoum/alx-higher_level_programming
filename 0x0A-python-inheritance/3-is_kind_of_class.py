c284e77c99e38fbd24a92cb49302aff1c94c8cac
unknown <aklileseyoum294@gmail.com>
2022-07-05 04:07:10 +0300

    3. Same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
     Module 3-is_kind_of_class
-    checks if the object is instance of
-    the class or its subclass
+    kind same instance
+    and methods
 """
 def is_kind_of_class(obj, a_class):
-    """check if its instance of the class"""
+     """ f the object is an instance of, or if the object is an
+    instance of a class that inherited from, the specified class """
     return isinstance(obj, a_class)
