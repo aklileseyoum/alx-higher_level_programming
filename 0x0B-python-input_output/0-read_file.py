5f7d2d115f95777f563de3cc477c8e2ceacccf37
unknown <aklileseyoum294@gmail.com>
2022-07-05 19:05:50 +0300

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 0-read_file
+reads file
+Method
+"""
+
+def read_file(filename=""):
+    """read the given file"""
+    with open(filename, encoding="utf-8") as myFile:
+        print(myFile.read())
