6bdd17c0427dc2332ae9b9883c19de7f673eaf9a
unknown <aklileseyoum294@gmail.com>
2022-07-05 19:21:25 +0300

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,7 @@ Method
 
 def write_file(filename="", text=""):
     """writes to a file and returns number of characters"""
+
     with open(filename, "w", encoding="utf-8") as myFile:
+        """opens a file in writing mode"""
         return myFile.write(text)
