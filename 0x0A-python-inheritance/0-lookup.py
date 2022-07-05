5af24a42d31edb469dbd90f2cdababa0aed86e94
unknown <aklileseyoum294@gmail.com>
2022-07-05 03:38:18 +0300

    0. Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def lookup(obj):
+    """list attributes and methods of a given object"""
     return dir(obj)
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
