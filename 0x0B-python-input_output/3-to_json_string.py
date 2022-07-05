52f353cc8ca75430ec13699960dec9641cb81fa4
unknown <aklileseyoum294@gmail.com>
2022-07-05 20:11:36 +0300

    3. To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import json
+"""
+Module 3-to_json_string
+change object to json
+method
+"""
+
+
+def to_json_string(my_obj):
+    """change object to json"""
+    return json.dumps(my_obj)
