3120c0525912c3289e0e2d6d27c43e3d4f1dbccb
unknown <aklileseyoum294@gmail.com>
2022-07-11 23:09:59 +0300

    commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,39 +9,39 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """method to initialize the variables"""
         super(). __init__(id=None)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     #getter method for width
     def get_width(self):
-        return self.__width
+        return self.width
 
     #setter method for width
     def set_width(self, width):
-        self.__width = width
+        self.width = width
 
     #getter method for height
     def get_height(self):
-        return self.__height
+        return self.height
 
     #setter method for height
     def set_height(self, h):
-        self.__height = h
+        self.height = h
 
     #getter method for x
     def get_x(self):
-        return self.__x
+        return self.x
 
     #setter method for x
     def set_x(self, x):
-        self.__x = x
+        self.x = x
 
     #getter method for y
     def get_y(self):
-        return self.__y
+        return self.y
 
     #setter method for y
     def set_y(self, y):
-        self.__y = y
+        self.y = y
