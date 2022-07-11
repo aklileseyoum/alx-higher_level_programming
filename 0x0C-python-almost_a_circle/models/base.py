bce6f07b46fd10efab58bc753e97d51671a5db9c
unknown <aklileseyoum294@gmail.com>
2022-07-11 22:25:59 +0300

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
+
+"""Defines a base model class."""
+
+
 class Base:
+     """Base model.
+    This Represents the "base" for all other classes in project 0x0C*.
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
+    """
+
     __nb_objects = 0
     def __init__(self, id = None):
+        """Initialize a new Base.
+        Args:
+            id (int): The identity of the new Base.
+        """
         if id != None:
             self.id = id
         else:
