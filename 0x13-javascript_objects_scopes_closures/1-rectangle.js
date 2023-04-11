1951ed3b5335728d24c80e1010951f4c25a9b5ad
unknown <aklileseyoum294@gmail.com>
2023-04-11 22:48:43 +0300

    commit

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
+//create a class with a constructor
 
 class Rectangle {
-	constructor(w, h) {
+	constructor (w, h) {
 		this.width = w;
 		this.height = h;
 	}
 }
+module.exports = Rectangle;
